8c3db636590bc6ff2370b2aa2d0990f109708be8
yi.yan11 <yi.yan11@600900B211DE405>
2018-03-29 11:58:38 +1100

    PlayerController

---
+++ Assets/~SunnyLand/Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SunnyLand
+{
+
+    public class PlayerController : MonoBehaviour
+    {
+
+        public float speed = 5f;
+        public int health = 100;
+        public int damage = 50;
+        public float hitForce = 4f;
+        public float damageForce = 4f;
+        public float maxVelocity = 4f;
+        public float maxSlopAngle = 45f;
+        [Header("Grounding")]
+        public float rayDistance = 0.5f;
+        public bool isGrounded = false;
+
+        [Header("Crouch")]
+        public bool isCrouching = false;
+
+        [Header("Jump")]
+        public float jumpHeight = 2f;
+        public int maxJumpCount = 2;
+        public bool isJumping = false;
+
+        private Vector3 groundNormal = Vector3.up;
+        private int currentJump = 0;
+        private float inputH, inputV;
+
+        //Refernces
+        private SpriteRenderer rend;
+        private Animator anim;
+        private Rigidbody2D rigid;
+
+        #region Unity Functions
+        void Awake()
+        {
+            rend = GetComponent<SpriteRenderer>();
+            anim = GetComponent<Animator>();
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        
+        void Update()
+        {
+
+
+        }
+
+        #endregion
+
+        #region Custom Functions
+        public void Jump()
+        {
+
+        }
+        public void Crouch()
+        {
+
+        }
+        public void UnCrouch()
+        {
+
+        }
+        public void Move(float horizontal)
+        {
+            
+        }
+        public void Hurt (int damage)
+        {
+
+        }
+
+        private void PerformClimb()
+        {
+
+        }
+        private void PerformMove()
+        {
+
+        }
+        private void PerformJump()
+        {
+
+        }
+
+        private void CheckGround(RaycastHit2D hit)
+        {
+
+        }
+        private void CheckEnemy(RaycastHit2D hit)
+        {
+
+        }
+        private void DetectClimbable()
+        {
+
+        }
+        private void DetectGround()
+        {
+
+        }
+        private void LimitVelocity()
+        {
+
+        }
+       
+        private void StopClimbing()
+        {
+
+        }
+        private void EnablePhysics()
+        {
+
+        }
+        private void DisablePhysics()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/~SunnyLand/Scripts/UserInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SunnyLand
+{
+
+    [RequireComponent(typeof(PlayerController))]
+
+    public class UserInput : MonoBehaviour
+    {
+        private PlayerController player;
+        private float inputH, inputV;
+        private bool isJumping = false, isCrouching = false;
+
+        void Start()
+        {
+            player = GetComponent<PlayerController>();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            GetInput();
+            player.Move(inputH);
+            if(isJumping)
+            {
+                player.Jump();
+            }
+            if(isCrouching)
+            {
+                player.Crouch();
+            }
+            else
+            {
+                player.UnCrouch();
+            }
+
+
+        }
+
+        void GetInput()
+        {
+            inputH = Input.GetAxis("Horizontal");
+            inputV = Input.GetAxis("Vertical");
+
+            isJumping = Input.GetKeyDown(KeyCode.Space);
+            isCrouching = Input.GetKeyDown(KeyCode.LeftControl);
+        }
+    }
+
+}
