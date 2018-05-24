75279880a31c55d33ad9e39e08421985603b9176
yi.yan11 <yi.yan11@600900B211DE405>
2018-05-24 12:01:41 +1000

    Driod, cam orbit, ai agent

---
+++ Unity/Assets/~TowerDefense/Scripts/Enemy/AIAgent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+
+public class AIAgent : MonoBehaviour
+{
+    public Transform target;
+
+    private NavMeshAgent nav;
+
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+       if(target != null)
+        {
+            nav.SetDestination(target.position);
+        }
+    }
+
+
+
+}
---
+++ Unity/Assets/~TowerDefense/Scripts/MouseOrbit.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+
+
+    public class MouseOrbit : MonoBehaviour
+    {
+
+        public Transform target;
+        public float distance = 5f;
+        public float xSpeed = 120f;
+        public float ySpeed = 120f;
+        public float yMinLimit = -20f;
+        public float yMaxLimit = 80f;
+        public float distanceMin = .5f;
+        public float distanceMax = 15f;
+
+        private float x = 0f, y = 0f;
+
+        public float ClampAngle(float angle, float min, float max)
+        {
+            if (angle < -360f)
+                angle += 360f;
+            if (angle > 360f)
+                angle -= 360f;
+            return Mathf.Clamp(angle, min, max);
+        }
+
+        void Start()
+        {
+            //Get the current rotation of the camera
+            Vector3 angles = transform.eulerAngles;
+            x = angles.y;
+            y = angles.x;
+        }
+
+        void Update()
+        {
+            //If target has been set and left mouse button is down
+            if(target && Input.GetMouseButton(1))
+            {
+                x += Input.GetAxis("Mouse X") * xSpeed * distance * Time.deltaTime;
+                y += Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
+
+                y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+                Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+                float scroll = Input.GetAxis("Mouse ScrollWheel");
+                distance = Mathf.Clamp(distance - scroll * 5, distanceMin, distanceMax);
+
+                Vector3 position = rotation * new Vector3(0, 0, -distance) + target.position;
+
+                transform.position = position;
+                transform.rotation = rotation;
+            }
+        }
+    }
+}
