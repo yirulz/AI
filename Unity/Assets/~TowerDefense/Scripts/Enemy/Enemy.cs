9878bbee2fc4f762c062688bb936f03bc82e11ca
yirulz <yirulz@hotmail.com>
2018-05-31 12:03:11 +1000

    Forgot to add files

---
+++ Unity/Assets/~TowerDefense/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TowerDefense
+{
+    public class Enemy : MonoBehaviour
+    {
+        public float maxHealth = 100;
+        [Header("UI")]
+        public GameObject healthBarUI;
+        public Vector2 healthBarOffset = new Vector2(0, 5f);
+
+        private Slider healthSlider;
+        private float health = 100f;
+
+        void Start()
+        {
+            health = maxHealth;
+        }
+
+
+        void Update()
+        {
+            //If health slider exists
+            if (healthSlider)
+            {
+                //Update health slider's position
+                healthSlider.transform.position = GetHealthBarPos();
+            }
+        }
+
+        Vector3 GetHealthBarPos()
+        {
+            Camera cam = Camera.main;
+            //Converts world position to screen
+            Vector3 position = cam.WorldToScreenPoint(transform.position);
+            //Returns screen position + offset (in pixels)
+            return position + (Vector3)healthBarOffset;
+        }
+
+        public void SpawnHealthBar(Transform parent)
+        {
+            GameObject clone = Instantiate(healthBarUI, GetHealthBarPos(), Quaternion.identity, parent);
+
+            //Store the slider component for later use
+            healthSlider = clone.GetComponent<Slider>();
+        }
+
+
+        public void DealDamage(float damage)
+        {
+            maxHealth -= damage;
+            
+
+            if(maxHealth <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
---
+++ Unity/Assets/~TowerDefense/Scripts/Spawners/EnemySpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+
+    public class EnemySpawner : Spawner
+    {
+        public float spawnRadius = 1f;
+        public Transform path;
+        [Header("UI")]
+        public Transform healthBarParent;
+
+        private Transform start;  //Spawn enemy
+        private Transform end;  //where enemy travels to
+        
+        void GetPath()
+        {
+            if(path != null)
+            {
+                start = path.FindChild("Start");
+                end = path.FindChild("End");
+                
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            GetPath();
+        }
+        
+        void OnDrawGizmos()
+        {
+            GetPath();
+            if(start != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(start.position, spawnRadius);
+            }
+        }
+
+        public override void Spawn()
+        {
+            GameObject clone = Instantiate(prefab, start.position, start.rotation);
+
+            clone.transform.SetParent(transform);
+
+            AIAgent agent = clone.GetComponent<AIAgent>();
+            agent.target = end;
+            //Spawn a health bar
+            Enemy enemy = clone.GetComponent<Enemy>();
+            enemy.SpawnHealthBar(healthBarParent);
+        }
+    }
+}
